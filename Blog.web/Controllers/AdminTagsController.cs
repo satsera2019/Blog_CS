5dba4fda90cc7f7a63b40c935025f683c9a15417
saba satseradze <saba.satseradze0@gmail.com>
2023-06-29 23:14:38 -0700

    Add: AdminTagController, Add view

---
+++ Blog.web/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
