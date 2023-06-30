274399b9eae44d8250871adff5095874c9ae2db7
saba satseradze <saba.satseradze0@gmail.com>
2023-06-29 20:16:30 -0700

    add BlogDbContext

---
+++ Blog.web/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
