90d44c743fa342d3b10d3417b9a34b7d7373b0ff
saba satseradze <saba.satseradze0@gmail.com>
2023-06-29 23:40:46 -0700

    add: AddTagRequest

--- Blog.web/Controllers/AdminTagsController.cs
+++ Blog.web/Controllers/AdminTagsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.web.Controllers
 {
@@ -8,6 +9,16 @@ namespace Blog.web.Controllers
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            return View("Add");
         }
     }
 }
---
+++ Blog.web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.web.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
