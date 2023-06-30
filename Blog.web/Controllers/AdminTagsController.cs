b8f18a3d2b6c6aa2dcfe31c6e75f257065bc34cb
saba satseradze <saba.satseradze0@gmail.com>
2023-06-30 00:05:34 -0700

    Add tag Done

--- Blog.web/Controllers/AdminTagsController.cs
+++ Blog.web/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using Blog.web.Models.ViewModels;
+﻿using Blog.web.Data;
+using Blog.web.Models.Domain;
+using Blog.web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -15,8 +24,15 @@ namespace Blog.web.Controllers
         [ActionName("Add")]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
 
             return View("Add");
         }
